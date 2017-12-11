25d8ac19021f56fbdcafc52918d22865b3ed70e8
Henrique Modesto da Costa <henriquemcosta@MacBook-de-Henrique-Modesto-da-Costa.local>
2017-12-11 20:27:00 -0200

    Primeiro sincronismo

---
+++ app/Http/Controllers/Categoria.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Categoria extends Controller
+{
+    public function categoria(){
+        return view('categoria');
+    }
+}
---
+++ resources/views/categoria.blade.php
@@ -0,0 +1,8 @@
+@extends('principal')
+@section('conteudo')
+<h1> Inseri Categoria</h1>
+<form>
+    <label>Nome</label>
+    <input/>
+</form>
+@stop
